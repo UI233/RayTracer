aafaa37e1089c329b551f84d7f33ea7f939ac0c1
UI233 <34206293+UI233@users.noreply.github.com>
2018-11-25 13:50:25 +0800

    Camera updated

--- Camera/Camera.cpp
+++ Camera/Camera.cpp
@@ -43,7 +43,7 @@ CUDA_FUNC void Camera::update()
         0.0f, 0.0f, 0.0f, 1.0f
     );
 
-    world2raster = scale(make_float3(1.0f / resolution.x, 1.0f / resolution.y, 1.0f))
+    world2raster = scale(make_float3(resolution.x, resolution.y, 1.0f))
         * pers * world2raster;
 
     raster2world = inverse(world2raster);
