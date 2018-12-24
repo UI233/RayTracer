70d18609f2b37f351f40d59544202a5ee4847c90
UI233 <34206293+UI233@users.noreply.github.com>
2018-12-24 09:04:33 +0800

    Strarified Sampler

--- src/RayTracer/Light/Light.cu
+++ src/RayTracer/Light/Light.cu
@@ -1,34 +1,20 @@
 #include "Light.cuh"
 
 CUDA_FUNC PointLight::PointLight(const float3 &position, const float3 &color) : pos(position), illum(color) {}
-CUDA_FUNC float3 PointLight::lightIllumi(Scene &scene, IntersectRecord &ref, Ray *wi, float2 sample) const 
+CUDA_FUNC float3 PointLight::lightIllumi(IntersectRecord &ref, Ray *wi, float2 sample) const 
 {
     *wi = Ray(pos, ref.pos - pos);
 
     float t1 = length(ref.pos - pos);
 
-    IntersectRecord rec;
-
-    scene.hit(*wi, rec);
-    
-    if (fabs(ref.t - t1) > 0.0001f)
-        return make_float3(0.0f);
-
-    return illum / dot(ref.pos - pos, ref.pos - pos);
+    return illum / fmaxf(0.001f, dot(ref.pos - pos, ref.pos - pos));
 }
 
 DirectionalLight::DirectionalLight(const float3 &direction, const float3 &color) : dir(normalize(direction)), illum(color) {}
 
-float3 DirectionalLight::lightIllumi(Scene &scene, IntersectRecord &ref, Ray *wi, float2 sample) const
+float3 DirectionalLight::lightIllumi(IntersectRecord &ref, Ray *wi, float2 sample) const
 {
     *wi = Ray(ref.pos - dir * 10000.0f, dir);
-
-    IntersectRecord rec;
-
-    scene.hit(*wi, rec);
-
-    if (length(rec.pos - ref.pos) > 0.0001f)
-        return make_float3(0.0f);
 
     return illum;
 }
