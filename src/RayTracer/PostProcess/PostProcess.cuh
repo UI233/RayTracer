a6ed615303ace3f14e883d4078cce9ee3bea9cf8
UI233 <34206293+UI233@users.noreply.github.com>
2019-01-09 21:27:34 +0800

    Add HDR kernel and Filter template

--- src/RayTracer/PostProcess/PostProcess.cuh
+++ src/RayTracer/PostProcess/PostProcess.cuh
@@ -25,7 +25,7 @@ inline __device__ float RGB2Y(const float3 &color)
 }
 
 //Incompleted
-CUDA_FUNC float3 filter(float3 color, float Ymax)
+CUDA_FUNC float3 HDR(float3 color, float Ymax)
 {	
 	float3 Y;
 	Y.x = 0.299f * color.x + 0.587f * color.y +
@@ -35,6 +35,8 @@ CUDA_FUNC float3 filter(float3 color, float Ymax)
 	Y.z = 0.615f * color.x - 0.515f * color.y -
 		0.1f * color.z;
 	float a;
+    if (Ymax == 0.0f)
+        return make_float3(0.0f, 0.0f, 0.0f);
 
 	Y.x = logf(Y.x) / logf(Ymax);
 
@@ -43,4 +45,45 @@ CUDA_FUNC float3 filter(float3 color, float Ymax)
 	R.y = fminf(1.0f, fmaxf(0.0f, 0.9996 * Y.x - 0.3954 * Y.y - 0.5805 * Y.z));
 	R.y = fminf(1.0f, fmaxf(0.0f, 1.002 * Y.x + 2.0361 * Y.y - 0.0005 * Y.z));
 	return R;
+}
+
+__global__ void HDRKernel(cudaSurfaceObject_t surface, cudaSurfaceObject_t surface_tmp, int width_per_thread, int height_per_thread, int width_per_block, int height_per_block,  float *Ymax)
+{
+    int stx = blockIdx.x * width_per_block + threadIdx.x * width_per_thread;
+    int sty = blockIdx.y * height_per_block + threadIdx.y * height_per_thread;
+
+    float4 color;
+    float3 tmp;
+    for(int x = stx;x < stx + width_per_thread; x++)
+        for (int y = sty; y < sty + height_per_thread; y++)
+        {
+            surf2Dread(&color, surface_tmp, x * sizeof(float), y);
+            tmp = HDR(make_float3(color.x, color.y, color.z), *Ymax);
+            surf2Dwrite(make_float4(tmp.x, tmp.y, tmp.z, 1.0f), surface, x * sizeof(float), y);
+        }
+
+}
+//Incompleted
+//
+__global__ void filterKernel(cudaSurfaceObject_t surface_tmp, int width_per_thread, int height_per_thread, int width_per_block, int height_per_block, int width, int height)
+{
+    int stx = blockIdx.x * width_per_block + threadIdx.x * width_per_thread;
+    int sty = blockIdx.y * height_per_block + threadIdx.y * height_per_thread;
+
+    float4 color;
+    float3 tmp;
+    float3 res;
+    for (int x = stx; x < stx + width_per_thread; x++)
+        for (int y = sty; y < sty + height_per_thread; y++)
+        {
+            res = make_float3(0.0f, 0.0f, 0.0f);
+            //3px x 3px window
+            for(int i = x - 1; i <= x + 1; i++)
+                for (int j = y - 1; j <= y + 1; j++)
+                {
+                    //surf2Dread(&color, surface_tmp, x * sizeof(float), y);
+
+                }
+            //surf2Dwrite(output , surface, x * sizeof(float), y);
+        }
 }
