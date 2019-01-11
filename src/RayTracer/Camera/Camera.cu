4b948a1c50a73f045ee3b13258b75336a5ea08f1
UI233 <34206293+UI233@users.noreply.github.com>
2019-01-12 00:47:13 +0800

    Camera updated

--- src/RayTracer/Camera/Camera.cu
+++ src/RayTracer/Camera/Camera.cu
@@ -8,47 +8,19 @@ CUDA_FUNC Camera::Camera(const float3 &pos, const float3 &lookat, const float &f
     pers(perspective(fov, (float)resolution.y / resolution.x, near, far)),
     resolution(res)
 {
-    float3 right = normalize(cross(front, up));
+    n = near;
+    right = normalize(cross(front, up));
     up = normalize(cross(right, front));
-    mat4 world2raster(
-        right.x, right.y, right.z, -pos.x,
-         up.x, up.y, up.z,-pos.y,
-        front.x, front.y, front.z, -pos.z,
-        0.0f, 0.0f, 0.0f, 1.0f
-    );
-
-    mat4 temp = scale(make_float3((float)resolution.x, (float)resolution.y, 1.0f));
-
-    world2raster = 
-        temp * pers * world2raster;
-    raster2world = inverse(world2raster);
 }
 
-CUDA_FUNC Ray Camera::generateRay(int x, int y)
+CUDA_FUNC Ray Camera::generateRay(float x, float y)
 {
     //float2 offset(sampler2D());
     //The direction of ray which computes the color of the pixel on (x,y)
-    float3 sample_direction(raster2world(make_float3(x, y, 0.0f)));
-
-    return Ray(pos, sample_direction - pos);
+    static const float step = 0.25f;
+    float aspect = (float)resolution.x / resolution.y; 
+    float3 dir = n * front + (float)x / resolution.x * right * step * aspect+ (float)y / resolution.y * up * step;
+    return Ray(pos, normalize(dir));
 }
 
 //CUDA_FUNC Ray generateDifferentialRay();
-CUDA_FUNC void Camera::update()
-{
-    float3 right = cross(front, up);
-
-    mat4 world2raster(
-        right.x, right.y, right.z, -pos.x,
-        up.x, up.y, up.z, -pos.y,
-        -front.x, -front.y, -front.z, -pos.z,
-        0.0f, 0.0f, 0.0f, 1.0f
-    );
-
-
-    mat4 temp = scale(make_float3((float)resolution.x, (float)resolution.y, 1.0f));
-
-    world2raster =
-        temp * pers * world2raster;
-    raster2world = inverse(world2raster);
-}
